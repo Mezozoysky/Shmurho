b713106ba5d4f07ab362d74967e3d090314878f1
Stanislav Demyanovich <s.demyanovich@socialquantum.com>
2019-10-21 11:29:44 +0700

    Add fetch-git-repo.cmake script, rewrite fetch-urho3d.cmake using collect_variables() function.

--- CMake/Scripts/fetch-git-repo.cmake
+++ CMake/Scripts/fetch-git-repo.cmake
@@ -1,8 +1,8 @@
 cmake_minimum_required(VERSION 3.13 FATAL_ERROR)
 
-if(NOT CMAKE_SCRIPT_MODE_FILE OR CMAKE_PARENT_LIST_FILE)
-    message(FATAL_ERROR "This file '${CMAKE_CURRENT_LIST_FILE}' should be used with 'cmake -P'")
-endif()
+ if(NOT CMAKE_SCRIPT_MODE_FILE OR CMAKE_PARENT_LIST_FILE)
+     message(FATAL_ERROR "This file '${CMAKE_CURRENT_LIST_FILE}' should be used with 'cmake -P'")
+ endif()
 
 get_filename_component(SCRIPT_NAME "${CMAKE_SCRIPT_MODE_FILE}" NAME)
 get_filename_component(PRJ_SCRIPTS_DIR "${CMAKE_SCRIPT_MODE_FILE}" DIRECTORY)
@@ -19,20 +19,20 @@ list(INSERT CMAKE_MODULE_PATH 0 "${PRJ_MODULES_DIR}")
 message("${SCRIPT_NAME} starts!")
 
 if(NOT REMOTE)
-    set(REMOTE "https://github.com/Mezozoysky/Urho3D.git")
+    message(FATAL_ERROR "${SCRIPT_NAME}:: Git remote isnt specified, use -DREMOTE")
 endif()
 if(NOT LOCAL)
-    set(LOCAL "${PRJ_DEPS_DIR}/Urho3D")
+    message(FATAL_ERROR "${SCRIPT_NAME}:: Git local directory isnt specified, use -DLOCAL")
 endif()
 
 include(ShmurhoGitUtils)
 
 fetch_git_repo(REMOTE ${REMOTE}
-               LOCAL ${LOCAL}
-               GIT_CMD "${GIT_CMD}"
-               BRANCH "${BRANCH}"
-               DEPTH "${DEPTH}"
-               SINGLE_BRANCH ${SINGLE_BRANCH}
-               )
+    LOCAL ${LOCAL}
+    GIT_CMD "${GIT_CMD}"
+    BRANCH "${BRANCH}"
+    DEPTH "${DEPTH}"
+    SINGLE_BRANCH ${SINGLE_BRANCH}
+    )
 
 message("${SCRIPT_NAME} is done!")
